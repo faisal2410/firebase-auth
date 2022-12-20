d097fcc9e2e8ee7f0977c3fa08f862af67c271cb
Faisal ahmed <faisal2410@yahoo.com>
2022-12-20 20:50:28 +0600

    updated

--- __step.js
+++ __step.js
@@ -29,3 +29,18 @@
 2. Create github, twitter, fb, etc. app create
 3. get clientId and secret 
 */
+
+
+/* 
+Auth Terminology:
+-------------------
+"Register" or "Sign up" [First time create a user]
+"log in" or "sign in" [Already you have a user account created]
+*/
+
+/* 
+    get form value: 
+    set form onSubmitHandler 
+    set event parameter
+    event.target.email.value
+*/
--- src/App.js
+++ src/App.js
@@ -1,72 +1,110 @@
-import { useState } from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
-import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
-import app from './firebase/firebase.init';
+import Login from './components/Login';
+import RegisterReactBootstrap from './components/RegisterReactBootstrap';
+import Main from './layout/Main';
 
-
-const auth=getAuth(app)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>
+      },
+      {
+        path: '/register',
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+    ]
+  }
+])
 
 function App() {
-  const [user, setUser] = useState({})
- 
-
-  const googleProvider=new GoogleAuthProvider();
-  const githubProvider = new GithubAuthProvider();
-const handleGoogleSignIn=()=>{
-  signInWithPopup(auth, googleProvider)
-  .then(result => {
-    const user = result.user;
-    setUser(user);
-    console.log(user);
-  })
-  .catch(error => {
-    console.error('error: ', error);
-  })
-}
-
-const handleSignOut = () => {
-  signOut(auth)
-    .then(() => {
-      setUser({});
-    })
-    .catch(() => {
-      setUser({})
-    })
-}
-
-const handleGithubSignIn= () =>{
-  signInWithPopup(auth, githubProvider)
-  .then( result => {
-    const user = result.user;
-    setUser(user);
-    console.log(user);
-  })
-  .catch( error =>{
-    console.error ('error: ', error)
-  })
-}
-
   return (
-    <div className="App">
-    {/* condition ? true: false */}
-
-    {
-      user.uid ?
-        <button onClick={handleSignOut}>Sign Out</button>
-        :
-        <>
-          <button onClick={handleGoogleSignIn}>Google Sign In</button>
-          <button onClick={handleGithubSignIn}>Github Sign IN</button>
-        </>
-
-    }
-    {user.uid && <div>
-      <h3>User name: {user.displayName}</h3>
-      <p>Email address: {user.email}</p>
-      <img src={user.photoURL} alt="" />
-    </div>}
-  </div>
+    <div className="">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
   );
 }
 
 export default App;
+
+// import { useState } from 'react';
+// import './App.css';
+// import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+// import app from './firebase/firebase.init';
+
+
+// const auth=getAuth(app)
+
+// function App() {
+//   const [user, setUser] = useState({})
+ 
+
+//   const googleProvider=new GoogleAuthProvider();
+//   const githubProvider = new GithubAuthProvider();
+// const handleGoogleSignIn=()=>{
+//   signInWithPopup(auth, googleProvider)
+//   .then(result => {
+//     const user = result.user;
+//     setUser(user);
+//     console.log(user);
+//   })
+//   .catch(error => {
+//     console.error('error: ', error);
+//   })
+// }
+
+// const handleSignOut = () => {
+//   signOut(auth)
+//     .then(() => {
+//       setUser({});
+//       console.log("Signout success")
+//     })
+//     .catch(() => {
+//       setUser({})
+//     })
+// }
+
+// const handleGithubSignIn= () =>{
+//   signInWithPopup(auth, githubProvider)
+//   .then( result => {
+//     const user = result.user;
+//     setUser(user);
+//     console.log(user);
+//   })
+//   .catch( error =>{
+//     console.error ('error: ', error)
+//   })
+// }
+
+//   return (
+//     <div className="App">
+//     {/* condition ? true: false */}
+
+//     {
+//       user.uid ?
+//         <button onClick={handleSignOut}>Sign Out</button>
+//         :
+//      <>
+//      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+//      <button onClick={handleGithubSignIn}>Github Sign IN</button>
+//      </>
+
+//     }
+//     {user.uid && <div>
+//       <h3>User name: {user.displayName}</h3>
+//       <p>Email address: {user.email}</p>
+//       <img src={user.photoURL} alt="" />
+//     </div>}
+//   </div>
+//   );
+// }
+
+// export default App;
